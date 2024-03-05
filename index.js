096347f3aef18f615286f46fbda16b91d8bd3ca9
Cacho Medina <zanlorenzocacho10@gmail.com>
2024-03-05 11:34:32 -0300

    configuracion de server, middlewares y practica de rutas

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const config = require("dotenv/config"); //permite procesar variables de entorno
 
 const app = express();
 
-const port = 3000;
+//setear variable de express
+app.set("port", process.env.PORT || 4000);
 
-app.get("/", (req, res) => {
-    res.setHeader({ "Content-Type": "text-plain/html" });
-    res.end();
+app.listen(app.get("port"), () => {
+    console.log(`servidor corriendo en puerto ${app.get("port")}`);
 });
 
-app.listen(port, () => {
-    console.log(`servidor corriendo en puerto ${port}`);
+app.use(cors()); //permite aceptar conexiones remotas
+app.use(morgan("dev")); //muestra informacion de las solicitudes
+app.use(express.json()); //permite interpretar el formato json
+app.use(express.urlencoded({ extended: true })); //permite interpretar los datos del body de una solicitud
+
+app.get("/", (req, res) => {
+    res.send("hola");
+});
+app.get("*", (req, res) => {
+    res.status(400).end("<h1>Error</h1>");
 });
