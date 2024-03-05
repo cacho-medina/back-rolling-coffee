a8bfbb8e17adf6d4b8b9c942b7a117f1dd15e5c2
Cacho Medina <zanlorenzocacho10@gmail.com>
2024-03-05 11:06:27 -0300

    se añadieron scripts

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("first message");
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.setHeader({ "Content-Type": "text-plain/html" });
+    res.end();
+});
+
+app.listen(port, () => {
+    console.log(`servidor corriendo en puerto ${port}`);
+});
