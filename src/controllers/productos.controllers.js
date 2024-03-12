91c24775d0ac6053695f447f1aefcc2e06ac9088
Cacho Medina <zanlorenzocacho10@gmail.com>
2024-03-12 11:32:37 -0300

    se creo el controllador de listar producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,7 +1,13 @@
 import Producto from "../database/models/producto.js";
 
-export const listarProd = (req, res) => {
-    res.send("Lista de Productos");
+export const listarProd = async (req, res) => {
+    try {
+        const listaProductos = await Producto.find();
+        res.status(200).json(listaProductos);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ message: "No se pudo obtener los productos" });
+    }
 };
 
 export const crearProd = async (req, res) => {
